a8fd2181eb487e49f321e67b4a8f8bb3d2a59826
Fred Yankowski <fcy@imsa.edu>
2016-05-03 19:23:59 -0500

    Use List instead of Dict to collect all pieces.

--- Main.elm
+++ Main.elm
@@ -1,6 +1,5 @@
 module Main exposing (..)
 
-import Dict exposing (Dict)
 import Html
 import Html.App as Html
 import Svg exposing (..)
@@ -21,12 +20,12 @@ main =
     }
 
 
-type alias PieceName =
+type alias Name =
   String
 
 
 type alias Model =
-  { pieces : Dict PieceName Piece.Model
+  { pieces : List ( Name, Piece.Model )
   }
 
 
@@ -34,42 +33,49 @@ init : ( Model, Cmd a )
 init =
   let
     pieces =
-      Dict.singleton "one" (Piece.init (Piece.Position 100 100))
+      [ ( "one", (Piece.init (Piece.Position 100 100)) ) ]
   in
     ( { pieces = pieces }, Cmd.none )
 
 
 type Msg
-  = PieceMsg PieceName Piece.Msg
+  = PieceMsg Name Piece.Msg
 
-
-updatePiece : Piece.Msg -> Maybe Piece -> Maybe Piece
-updatePiece msg pieceMaybe =
-  Maybe.map (Piece.update pieceMsg) pieceMaybe
-       
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    PieceMsg pieceName pieceMsg ->
+    PieceMsg name pieceMsg ->
       let
-        pieces' = Dict.update pieceName (updatePiece pieceMsg) pieces
+        ( pieces', cmds ) =
+          updatePieces name pieceMsg model.pieces
       in
-        case pieceMaybe of
-          Nothing ->
-            Debug.crash "nothing for piece"
+        ( { model | pieces = pieces' }, Cmd.batch cmds )
 
-          Just piece ->
-            let
-              ( piece', cmd ) = Piece.update pieceMsg piece
-              pieces 
-            in
-              ( { model | piece = piece' }, cmd |> Cmd.map PieceMsg )
+
+updatePieces : Name -> Piece.Msg -> List ( Name, Piece.Model ) -> ( List ( Name, Piece.Model ), List (Cmd Msg) )
+updatePieces name msg items =
+  let
+    updatePiece (( pieceName, piece ) as item) ( items, cmds ) =
+      if pieceName == name then
+        let
+          ( piece', cmd ) =
+            Piece.update msg piece
+        in
+          ( ( name, piece' ) :: items, (Cmd.map (PieceMsg name) cmd) :: cmds )
+      else
+        ( item :: items, cmds )
+  in
+    List.foldl updatePiece ( [], [] ) items
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Piece.subscriptions model.piece |> Sub.map PieceMsg
+  let
+    mapSubs ( name, piece ) =
+      Piece.subscriptions piece |> Sub.map (PieceMsg name)
+  in
+    List.map mapSubs model.pieces |> Sub.batch
 
 
 
@@ -89,5 +95,9 @@ scene : Model -> Html.Html Msg
 scene model =
   Svg.svg
     [ width "600", height "600" ]
-    [ Piece.view model.piece |> Html.map PieceMsg
-    ]
+    (List.map pieceView model.pieces)
+
+
+pieceView : ( Name, Piece.Model ) -> Svg.Svg Msg
+pieceView ( name, piece ) =
+  Piece.view piece |> Html.map (PieceMsg name)
